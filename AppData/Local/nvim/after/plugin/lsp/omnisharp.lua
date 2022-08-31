6a774d9ef9e4486e7f3d0afe8c37aeb01fd0944e
denco <dlochman@sygic.com>
2022-08-31 10:58:03 +0200

    Adapted function that registers lsp server to better handle additional settings

--- AppData/Local/nvim/after/plugin/lsp/omnisharp.lua
+++ AppData/Local/nvim/after/plugin/lsp/omnisharp.lua
@@ -1,3 +1,4 @@
-local server = require("denco.lsp_server").setup_server("omnisharp", { "dotnet", "C:/omnisharp/OmniSharp.dll" })
-
-server.document_config.default_config.analyze_open_documents_only = true
+require("denco.lsp_server").setup_server(
+    "omnisharp",
+    { "dotnet", "C:/omnisharp/OmniSharp.dll" },
+    { analyze_open_documents_only = true })
--- AppData/Local/nvim/lua/denco/lsp_server.lua
+++ AppData/Local/nvim/lua/denco/lsp_server.lua
@@ -1,25 +1,29 @@
 local M = {}
 
-local nvim_lsp = require("lspconfig")
+local lspconfig = require("lspconfig")
 local lsp_signature = require("lsp_signature")
 local nnoremap = require("denco.keymap").nnoremap
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-function M.setup_server(server_name, cmd, settings)
-    local server = nvim_lsp[server_name]
-    server.setup {
+function M.setup_server(server_name, cmd, other_settings)
+    local server = lspconfig[server_name]
+
+    local setup = {
         cmd = cmd or server.document_config.default_config.cmd,
         capabilities = capabilities,
         on_attach = function(_, bufnr)
             lsp_signature.on_attach(lsp_signature, bufnr)
             nnoremap('<leader>rn', vim.lsp.buf.rename)
             nnoremap('<leader>a', vim.lsp.buf.code_action)
-        end,
-        settings = settings or server.document_config.default_config.settings
+        end
     }
 
-    return server
+    if other_settings == nil then
+        server.setup(setup)
+    else
+        server.setup(vim.tbl_extend("force", setup, other_settings))
+    end
 end
 
 return M
