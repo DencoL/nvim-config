46a9c874ff13e28e6b803f751c0aa2f3f1a6e1dd
denco <dlochman@sygic.com>
2022-10-31 15:53:54 +0100

    Changed live_grep keybinding, added fuzzy find for current buffer

--- AppData/Local/nvim/after/plugin/keymaps/telescope.lua
+++ AppData/Local/nvim/after/plugin/keymaps/telescope.lua
@@ -2,7 +2,8 @@ local nnoremap = require("denco.keymap").nnoremap
 
 nnoremap("<leader>n", "<cmd>lua require('telescope.builtin').find_files()<CR>")
 nnoremap("<leader>m", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
-nnoremap("<leader>f", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nnoremap("<leader>fn", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nnoremap("<leader>fm", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>")
 
 nnoremap("gu", "<cmd>lua require('telescope.builtin').lsp_references()<CR>")
 nnoremap("gd", "<cmd>lua require('denco.utils').go_to_definition()<CR>")
