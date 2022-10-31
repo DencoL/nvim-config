bd27e991cd5841dd5195501736ba17c64ae6af9c
denco <dlochman@sygic.com>
2022-10-31 15:53:09 +0100

    Added 'target' folder to be ignored by find_files

--- AppData/Local/nvim/after/plugin/setups/telescope.lua
+++ AppData/Local/nvim/after/plugin/setups/telescope.lua
@@ -1,6 +1,6 @@
 require("telescope").setup{
     defaults = {
-        file_ignore_patterns = { ".git", ".idea", "obj", "bin" },
+        file_ignore_patterns = { ".git", ".idea", "obj", "bin", "target" },
         path_display = function(_, path)
             local tail = require("telescope.utils").path_tail(path)
             return string.format("%s | %s", tail, vim.fn.fnamemodify(path, ":h"))
