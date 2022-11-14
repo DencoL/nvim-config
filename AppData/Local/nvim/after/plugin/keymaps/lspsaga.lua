3d06a62b0ae6254949f862e7af0df5614fbc06de
denco <dlochman@sygic.com>
2022-11-14 11:44:48 +0100

    added forgotten lspsaga keymaps

--- AppData/Local/nvim/after/plugin/keymaps/lspsaga.lua
+++ AppData/Local/nvim/after/plugin/keymaps/lspsaga.lua
@@ -2,4 +2,3 @@ local nnoremap = require("denco.keymap").nnoremap
 
 nnoremap("<leader>rn", "<cmd>Lspsaga rename<CR>")
 nnoremap("<leader>a", "<cmd>Lspsaga code_action<CR>")
-nnoremap("<leader>a", "<cmd>Lspsaga code_action<CR>")
